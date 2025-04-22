6053e03cc1cae0a4cf7b08ea46e0954da4ec8691
shaoqisama <562334830@qq.com>
2025-04-22 17:05:48 +0800

    Configure project for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  // Configure for GitHub Pages
+  basePath: process.env.GITHUB_ACTIONS && '/music-fx-generator',
+  assetPrefix: process.env.GITHUB_ACTIONS && '/music-fx-generator',
 }
 
 export default nextConfig
